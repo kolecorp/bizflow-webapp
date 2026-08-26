e5db664b929994481f4465912ecfe289de835e77
Adakol3 <realdxihub@gmail.com>
2026-08-26 03:37:04 +0100

    Delete .svelte-kit directory

--- .svelte-kit/generated/client/nodes/12.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/(app)/services/+page.svelte";
--- .svelte-kit/generated/client/nodes/8.js
+++
@@ -1,2 +0,0 @@
-import * as universal from "../../../../src/routes/(app)/overview/+page.ts";
-export { universal };
